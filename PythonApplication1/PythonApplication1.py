cb796f11b2b6021fc604b3026fac4077471a247c
stsluk <yubinibini12357@gmail.com>
2023-07-12 19:41:27 +0900

    chang commit

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,48 +1,119 @@
 from tkinter import *
-import time
+from time import sleep
 import math
+from random import randrange, choice
+import sys
 
 window = Tk()
-window.title("game")
+window.title("nemoRPG")
 window.resizable(0, 0)
 canvas = Canvas(window, width=640, height=640, bg="white")
 canvas.pack()
+objects, enemyObjects, score = set(), set(), 0
 
 class Game:
-    global objects
-    objects = set()
     def __init__(self):
-        self.keys = set()
-        self.mx, self.my, self.mPressed = 0, 0, 0
+        global objects, enemyObjects, score
+        self.keys = set()   # 버튼 세트
+        self.mx, self.my, self.mPressed = 0, 0, 0   # 마우스 좌표, 클릭 여부
+        self.runtime, self.spontime = 0, 300
+        self.hp_before, self.score_before = 0, 0
+        self.enemyvalue = [[8, "red"], [15, "blue"], [10, "green"], [25, "gold"]]
         window.bind("<KeyPress>", self.keyPressHandler)
         window.bind("<KeyRelease>", self.keyReleaseHandler)
         canvas.bind("<Button-1>", self.mousePress)
         canvas.bind("<B1-Motion>", self.mousePress)
         canvas.bind("<ButtonRelease-1>", self.mouseRelease)
-        canvas.pack
+        canvas.pack()
         
-        obj_main = element(310, 310, 20, 20, "black")
+        canvas.create_rectangle(0, 0, 640, 640, fill="black")
+        canvas.create_text(320, 60, text="네모네모 슈팅 게임", fill="white", font=("둥근모꼴", 28))
+        canvas.create_rectangle(100, 200, 135, 235, fill="gray22", outline="white")
+        canvas.create_text(117, 217, text="A", fill="white", font=("둥근모꼴", 16))
+        canvas.create_rectangle(143, 200, 178, 235, fill="gray22", outline="white")
+        canvas.create_text(160, 217, text="S", fill="white", font=("둥근모꼴", 16))
+        canvas.create_rectangle(186, 200, 221, 235, fill="gray22", outline="white")
+        canvas.create_text(203, 217, text="D", fill="white", font=("둥근모꼴", 16))
+        canvas.create_rectangle(143, 157, 178, 192, fill="gray22", outline="white")
+        canvas.create_text(160, 174, text="W", fill="white", font=("둥근모꼴", 16))
+        canvas.create_text(160, 270, text="< 이동 키 >", fill="white", font=("둥근모꼴", 12))
+        canvas.create_rectangle(450, 157, 510, 235, outline="white")
+        canvas.create_line(480, 187, 510, 187, fill="white")
+        canvas.create_rectangle(450, 157, 480, 187, fill="gray22", outline="white")
+        canvas.create_text(480, 270, text="< 공격 키 >", fill="white", font=("둥근모꼴", 12))
+        canvas.create_text(300, 400, text="   사방에서 몰려오는 네모들을 처치하고 점수를 얻으세요!\n\n   시간이 지날수록 강한 적들이 등장합니다.", fill="white", font=("둥근모꼴", 12))
+        self.textblinker()   # 시작 대기
+
+        obj_main = object_main(310, 310, 20, 20, "black")
+
+        score_view = canvas.create_text(540, 15, text=f"SCORE: {score}", font=("나눔고딕코딩", 12), fill="red")
+        canvas.create_rectangle(5, 5, 420, 25, fill="gray82", width=0)
+        hpbar = canvas.create_rectangle(5, 5, 420, 25, fill="springGreen2", width=0)
+        hptext = canvas.create_text(200, 15, text=f"HP: ({obj_main.hp} / 1000)", font=("나눔고딕코딩", 8))
         
-        while True:
-            for key in self.keys:
-                if key == ord('A') and obj_main.x_accel > -10:
-                    obj_main.x_accel -= 1
-                elif key == ord('D') and obj_main.x_accel < 10:
-                    obj_main.x_accel += 1
-                elif key == ord('W') and obj_main.y_accel > -10:
-                    obj_main.y_accel -= 1
-                elif key == ord('S') and obj_main.y_accel < 10:
-                    obj_main.y_accel += 1
+        while True:   # 메인 루프
+            if obj_main in objects:
+                for key in self.keys:
+                    if key == ord('A') and obj_main.x_accel > -3:
+                        obj_main.x_accel -= 1
+                    elif key == ord('D') and obj_main.x_accel < 3:
+                        obj_main.x_accel += 1
+                    elif key == ord('W') and obj_main.y_accel > -3:
+                        obj_main.y_accel -= 1
+                    elif key == ord('S') and obj_main.y_accel < 3:
+                        obj_main.y_accel += 1
             
-            if self.mPressed == 1:
-                obj_attack = object_attack(canvas.coords(obj_main.canvas_id)[0]+8, canvas.coords(obj_main.canvas_id)[1]+8, 4, 4, "black", 120)
-                obj_attack.x_accel, obj_attack.y_accel = self.movePoint(canvas.coords(obj_attack.canvas_id)[0]+10, canvas.coords(obj_attack.canvas_id)[1]+10, self.mx, self.my, 25)
+                if self.mPressed == 1 and obj_main.coolt == obj_main.cool:   # 마우스 클릭 시
+                    obj_attack = object_attack(canvas.coords(obj_main.canvas_id)[0]+8, canvas.coords(obj_main.canvas_id)[1]+8, 4, 4, "black", 120)
+                    obj_attack.x_accel, obj_attack.y_accel = self.movePoint(canvas.coords(obj_attack.canvas_id)[0]+10, canvas.coords(obj_attack.canvas_id)[1]+10, self.mx, self.my, 25)
+                    obj_main.coolt = 0
+
+                if self.hp_before != obj_main.hp:
+                    canvas.delete(hpbar)
+                    canvas.delete(hptext)
+                    hpbar = canvas.create_rectangle(5, 5, 420 * obj_main.hp / obj_main.mhp, 25, fill="springGreen2", width=0)
+                    hptext = canvas.create_text(200, 15, text=f"HP: ({obj_main.hp} / 1000)", font=("나눔고딕코딩", 8))
+                    self.hp_before = obj_main.hp
+
+                if self.score_before != score:
+                    canvas.itemconfig(score_view, text=f"SCORE: {score}")
+                    self.score_before = score
+
+                self.runtime += 1
+                if len(enemyObjects) < 25 and self.runtime % self.spontime == 0:
+                        for i in range(4):
+                            if self.runtime % (self.spontime * (i+1)**2) == 0:
+                                obj_enemy = object_enemy(choice([-100, 740])+randrange(-50, 50), choice([-100, 740])+randrange(-50, 50), 
+                                                     self.enemyvalue[i][0], self.enemyvalue[i][0], self.enemyvalue[i][1], obj_main, i)
+                        self.spontime = max([randrange(self.spontime - 2, self.spontime), 50])   # 스폰타임 초기화
+
+                for obj in enemyObjects.copy():
+                    degree = math.atan2(canvas.coords(obj_main.canvas_id)[0] - canvas.coords(obj.canvas_id)[0], canvas.coords(obj_main.canvas_id)[1] - canvas.coords(obj.canvas_id)[1])
+                    obj.x_accel, obj.y_accel = - obj.enemy_stat[obj.enemy_type][1] * math.cos(degree), 5 * math.sin(degree)
+                    if obj.coolt == obj.cool:
+                        obj_enemyAttack = object_enemyAttack(canvas.coords(obj.canvas_id)[0] + (obj.size_x-obj.enemy_stat[obj.enemy_type][3])/2, 
+                                                          canvas.coords(obj.canvas_id)[1]+(obj.size_y-obj.enemy_stat[obj.enemy_type][3])/2, 
+                                                          obj.enemy_stat[obj.enemy_type][3], obj.enemy_stat[obj.enemy_type][3], obj.color, 100, obj_main, obj.enemy_stat[obj.enemy_type][5])
+                        obj_enemyAttack.x_accel, obj_enemyAttack.y_accel = self.movePoint(canvas.coords(obj_enemyAttack.canvas_id)[0]+randrange(-5, 5), 
+                                                                                          canvas.coords(obj_enemyAttack.canvas_id)[1]+randrange(-5, 5), canvas.coords(obj_main.canvas_id)[0]+10, 
+                                                                                          canvas.coords(obj_main.canvas_id)[1]+10, obj. enemy_stat[obj.enemy_type][4])
+                        obj.coolt = 0
             
             for obj in objects.copy():
+                obj.move()
                 obj.step()
+
+            if not obj_main in objects:
+                canvas.delete("all")
+                break
             
             window.update()
-            time.sleep(0.01)
+            sleep(0.01)
+
+        canvas.create_text(320, 260, text="Game Over...", fill="red", font=("둥근모꼴", 38))
+        canvas.create_text(320, 370, text=f"{score}점", fill="black", font=("둥근모꼴", 28))
+        self.textblinker("exit")
+        sys.exit(1)
             
     def keyPressHandler(self, event):
         self.keys.add(event.keycode)
@@ -59,10 +130,23 @@ class Game:
     
     def movePoint(self, x1, y1, x2, y2, spd):
         distance = math.sqrt((x2-x1)**2 + (y2-y1)**2)
-        if spd < distance:
-            return (x2-x1)*spd / distance, (y2-y1)*spd / distance
-        else:
-            return 0, 0
+        return (x2-x1)*spd / distance, (y2-y1)*spd / distance
+
+    def textblinker(self, sentance = "start"):
+        menuToggle = True
+        blinkerText = canvas.create_text(320, 580, text="< Please press spacebar to " + sentance + ". >", fill="red", font=("둥근모꼴", 12))
+        while True:
+            self.runtime += 1
+            for key in self.keys:
+                if key == 32:
+                    self.runtime = 0
+                    canvas.delete("all")
+                    return 
+            if self.runtime % 60 == 0:
+                if menuToggle: canvas.itemconfig(blinkerText, text=""); menuToggle = False
+                else: canvas.itemconfig(blinkerText, text="< Please press spacebar to " + sentance + ". >"); menuToggle = True
+            window.update()
+            sleep(0.01)
 
 
 class element:
@@ -95,35 +179,74 @@ class element:
                 y_value = 1280 - 2*canvas.coords(self.canvas_id)[3] - y_value
                 self.y_accel *= -1
             canvas.move(self.canvas_id, x_value, y_value)
-            self.x_accel -= self.x_accel/50
-            self.y_accel -= self.y_accel/50
-    
-    def step(self):
-        self.move()
+            self.mx, self.my = 0, 0
+            self.x_accel *= 0.98
+            self.y_accel *= 0.98
+
+    def collision(self, obj):
+        return True if canvas.coords(self.canvas_id)[0] <= canvas.coords(obj.canvas_id)[2] and canvas.coords(self.canvas_id)[2] >= canvas.coords(obj.canvas_id)[0] and canvas.coords(self.canvas_id)[1] <= canvas.coords(obj.canvas_id)[3] and canvas.coords(self.canvas_id)[3] >= canvas.coords(obj.canvas_id)[1] else False
 
 
 class object_main(element):
     def __init__(self, x, y, size_x, size_y, color):
         super().__init__(x, y, size_x, size_y, color)
         self.mhp, self.hp = 1000, 1000
-        self.cool, self.coolt = 25, 0
+        self.cool, self.coolt = 5, 0
     
     def step(self):
-        self.move()
-        if self.coolt < self.cool:
-            self.coolt += 1
+        global score
+        if self.coolt < self.cool: self.coolt += 1
+        if self.hp <= 0: self.destroy()
+
+
+class object_enemy(element):
+    def __init__(self, x, y, size_x, size_y, color, obj_main, enemy_type):
+        super().__init__(x, y, size_x, size_y, color)
+        self.enemy_stat = [[100, 2, 30, 3, 10, 15], [500, 1, 75, 5, 11, 50], 
+                           [150, 3, 10, 3, 15, 8], [2500, 1, 30, 6, 12, 33]]   # HP, 속도, 공격속도, 투사체 크기, 투사체 속도, 데미지
+        self.enemy_type = enemy_type
+        self.mhp = self.enemy_stat[self.enemy_type][0]
+        self.hp = self.mhp
+        self.cool, self.coolt = self.enemy_stat[self.enemy_type][2], 0
+        enemyObjects.add(self)
+        self.obj_main = obj_main
+
+    def step(self):
+        if self.coolt < self.cool: self.coolt += 1
+        if self.hp <= 0:
+            global score
+            score += self.mhp
+            enemyObjects.discard(self)
+            self.destroy()
 
 
 class object_attack(element):
     def __init__(self, x, y, size_x, size_y, color, livetime):
         super().__init__(x, y, size_x, size_y, color)
-        self.livetime = livetime
-        self.fortime = 0
+        self.livetime, self.fortime = livetime, 0
     
     def step(self):
-        self.move()
-        if self.livetime <= self.fortime:
+        for obj_s in enemyObjects:
+            if self.collision(obj_s):
+                obj_s.hp -= 50
+                self.destroy();
+                break
+        if self.livetime <= self.fortime: self.destroy()
+        self.fortime += 1
+
+
+class object_enemyAttack(element):
+    def __init__(self, x, y, size_x, size_y, color, livetime, obj_main, damage):
+        super().__init__(x, y, size_x, size_y, color)
+        self.livetime, self.fortime = livetime, 0
+        self.obj_main = obj_main
+        self.damage = damage
+
+    def step(self):
+        if self.obj_main in objects and self.collision(self.obj_main):
+            self.obj_main.hp -= self.damage
             self.destroy()
+        if self.livetime <= self.fortime: self.destroy()
         self.fortime += 1
 
 
